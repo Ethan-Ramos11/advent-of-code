6f6addd434b9d7dd1dbec6af2020a25cb028d447
Ethan Ramos <ethan.n.ramos@gmail.com>
2025-04-14 19:06:00 -0400

    completed day one part two

--- dayone.py
+++ dayone.py
@@ -11,8 +11,8 @@ def read_in(path):
     return lst_one, lst_two
 
 
-def find_distance():
-    lst_one, lst_two = read_in("dayOneInput.txt")
+def find_distance(file_path):
+    lst_one, lst_two = read_in(file_path)
     lst_one.sort()
     lst_two.sort()
 
@@ -22,4 +22,25 @@ def find_distance():
     return distance
 
 
-print(find_distance())
+def generate_keys(lst):
+    dict = {}
+    for num in lst:
+        if num not in dict:
+            dict[num] = 0
+    return dict
+
+
+def find_similarity_score(file_path):
+    lst_one, lst_two = read_in(file_path)
+    occurence = generate_keys(lst_one)
+    for num in lst_two:
+        if num in occurence:
+            occurence[num] += 1
+    score = 0
+    for n, val in occurence.items():
+        score += n * val
+    return score
+
+
+print(find_distance("dayOneInput.txt"))
+print(find_similarity_score("dayOneInput.txt"))
